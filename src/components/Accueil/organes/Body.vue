d84831c77348a837e92f88dd4333f911ec4b4ed0
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-04-03 22:39:23 +0300

    Rendez-vous terminer

--- src/components/Accueil/organes/Body.vue
+++ src/components/Accueil/organes/Body.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import Button from "../molecules/Button.vue";
+    import validation from '../icon/SVG/validation.svg';
 </script>
 <template>
     <div class="bloc caracteristique" id="caracteristique">Caractéristiques</div>
@@ -14,6 +14,9 @@
             <div class="corp">
                 <h2>Prêt à vivre l’expérience 
                     <br>tout-terrain ultime ?</h2>
+                <p>Prenez rendez-vous pour un essai 
+                    <br>gratuit en magasin et ressentez 
+                    <br>la différence !</p>
             </div>
             <div class="formulaire">
                 <form action="#">
@@ -25,11 +28,17 @@
                     <input type="number" placeholder="+261" style="width: 15%;">
                     <input type="number" placeholder="Numero telephone" style="width: 70%; margin-left: -2px;">
                         <br>
-                    <input type="date" placeholder="Date de Rendez-vous xx/xx/xxxx  - 00:00" style="width: 87%;">
+                    <input type="date" style="width: 87%;">
                         <br>
-                    <input type="button" value="Je prends rendez-vous">
+                    <input type="button" value="Je prends rendez-vous" style="margin-top: 30px; height: 90px; width: 50%;">
                 </form>
+                <div class="popup-validation">
+                    <div style="width: 100%;height: 100%; position: absolute; background-color: #E6DBD2; opacity: 26%;"></div>
+                    <img :src="validation" alt="Next Icon" class="icon" />
+                    <p>Rendez-vous Valider</p>
+                </div>
             </div>
+            <div class="ligne" style="position: absolute; background-color: white; width: 81%; height: 1px; margin-top: -70px;"/>
         </div>
     </div>
     <div class="bloc velo" id="velo">Vélo</div>
@@ -55,9 +64,10 @@
 }
 
 .rendez-vous {
-    background-color: #0C0C0C; /* Vert */
+    background-color: #0C0C0C;
 }
     .rendez-vous .container{
+        margin-top: -50px;
         margin-left: 100px;
     }
     .rendez-vous .container .title h1{
@@ -78,9 +88,16 @@
         font-weight: 300;
         color: #FFFFFF;
     }
+    .rendez-vous .container .corp p{
+        font-family: 'Poppins','sans-serif';
+        font-weight: 100;
+        font-style: italic;
+        font-size: 15px;
+        color: #FFFFFF;
+    }
     .rendez-vous .container .formulaire{
         position: relative;
-        margin-top: -230px;
+        margin-top: -300px;
         margin-left: 130%;
         width: 500px;
         height: 500px;
@@ -88,6 +105,10 @@
     }
         .formulaire input::placeholder {
             padding-left: 20px;
+            text-indent: 1px;
+        }
+        .formulaire input:focus  {
+            padding-left: 2px;
         }
         .rendez-vous .container .formulaire form input{
             font-family: 'Poppins';
@@ -102,6 +123,25 @@
             border: 1px solid #FFFFFF; /* Ajoute une bordure légère pour visibilité */
             border-radius: 5px;
         }
+        .rendez-vous .container .formulaire .popup-validation{
+            position: absolute;
+            width: 500px;
+            height: 300px;
+            margin-left: -350px;
+            margin-top: -350px;
+            text-align: center;
+            font-family: 'Orbitron','sans-serif';
+            font-size: 25px;
+            font-weight: 200;
+            color: #FFFFFF;
+            visibility: hidden;
+            /* eto izao .... */
+        }
+.icon {
+    width: 200px;
+    height: 200px;
+    margin-bottom: -20px;
+}
 
 .velo {
     background-color: #5733FF; /* Bleu violet */
